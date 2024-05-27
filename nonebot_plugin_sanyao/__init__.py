0ee930f49d326be68779b1c265ae5f78ef638f7b
afterow <afterow@163.com>
2024-05-27 13:58:21 +0800

    项目初始化

--- nonebot_plugin_sanyao/__init__.py
+++ nonebot_plugin_sanyao/__init__.py
@@ -4,14 +4,14 @@ from nonebot.adapters import Message
 from nonebot.params import CommandArg
 from .sanyao import print_lunar_info_and_bazi
 
-weather = on_command("三爻", rule=to_me(), aliases={"三爻排盘", "三爻易"}, priority=10, block=True)
+sanyaopaipan = on_command("三爻", rule=to_me(), aliases={"三爻排盘", "三爻易"}, priority=10, block=True)
 
-@weather.handle()
+@sanyaopaipan.handle()
 async def handle_function(args: Message = CommandArg()):
     # 提取参数纯文本作为地名，并判断是否有效
     if location := args.extract_plain_text():
         xx=print_lunar_info_and_bazi(location)
-        await weather.finish(xx)
+        await sanyaopaipan.finish(xx)
     else:
-        await weather.finish("请输入参数 如/三爻 101")
+        await sanyaopaipan.finish("请输入参数 如/三爻 101")
 
